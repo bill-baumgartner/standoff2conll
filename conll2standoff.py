6b63cdff40dcca17247b83c2bcd907529d74f947
Lenz <Lenz.Furrer@gmail.com>
2019-06-11 10:58:21 +0200

    additional script for inverse conversion

---
+++ conll2standoff.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf8
+
+
+from __future__ import unicode_literals
+
+
+import sys
+
+from document import Document
+
+
+def main():
+    '''
+    Run as script.
+    '''
+    text = ''
+    for line in sys.stdin:
+        # Decode and flip token and tag.
+        line = line.decode('utf8')
+        if not line.isspace():
+            token, start, end, tag = line.rstrip().split('\t')
+            if tag != 'O-NIL' and tag.startswith('O'):
+                tag = 'I' + tag[1:]
+            line = '{}\t{}\t{}\t{}\n'.format(tag, start, end, token)
+        text += line
+
+    doc = Document.from_nersuite(text)
+    sys.stdout.write(doc.to_standoff().encode('utf8'))
+
+
+if __name__ == '__main__':
+    main()
