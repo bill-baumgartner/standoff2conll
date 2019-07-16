4f402e4c835b864056e734fe1b40552faaf774e4
Lenz <Lenz.Furrer@gmail.com>
2019-07-16 11:51:39 +0200

    fix conll2standoff.py: be robust wrt invalid tag sequences

--- conll2standoff.py
+++ conll2standoff.py
@@ -2,10 +2,22 @@
 # coding: utf8
 
 
+"""
+Convert CoNLL to BioNLP standoff.
+
+Accept IO, IOB, and IOBES tags.
+Be graceful about tag-sequence errors.
+
+Input needs at least 4 tab-separated columns:
+    token, start offset, end offset, tag [, more...]
+"""
+
+
 from __future__ import unicode_literals
 
 
 import sys
+import codecs
 
 from document import Document
 
@@ -14,19 +26,63 @@ def main():
     '''
     Run as script.
     '''
-    text = ''
-    for line in sys.stdin:
-        # Decode and flip token and tag.
-        line = line.decode('utf8')
-        if not line.isspace():
-            token, start, end, tag = line.rstrip().split('\t')
-            if tag.startswith('O') and tag not in ('O', 'O-NIL'):
-                tag = 'I' + tag[1:]
-            line = '{}\t{}\t{}\t{}\n'.format(tag, start, end, token)
-        text += line
+    conll2standoff(codecs.getreader('utf8')(sys.stdin),
+                   codecs.getwriter('utf8')(sys.stdout))
 
+
+def conll2standoff(src, tgt):
+    """Convert CoNLL with character offsets to BioNLP standoff."""
+    rows = parse_conll(src)
+    rows = reformat(rows)
+    text = '\n'.join('\t'.join(r) for r in rows) + '\n'
     doc = Document.from_nersuite(text)
-    sys.stdout.write(doc.to_standoff().encode('utf8'))
+    tgt.write(doc.to_standoff())
+
+
+def parse_conll(lines):
+    """Parse CoNLL TSV."""
+    for line in lines:
+        if not line.lower().startswith('# doc_id'):
+            line = line.rstrip()
+            if line:
+                yield line.split('\t')
+            else:
+                yield []
+
+
+def reformat(rows):
+    """Reformat CoNLL input to well-formed NERSuite format."""
+    last = OUTSIDE[0]
+    for row in rows:
+        if not row:
+            last = OUTSIDE[0]
+        else:
+            # Flip token/tag and sanitise the tag sequence.
+            tag = row[3]
+            tag = last = fix_tag(tag, last)
+            row[3] = row[0]
+            row[0] = tag
+        yield row
+
+
+OUTSIDE = ('O', 'O-NIL')
+INSIDE = ('I', 'E')
+BEGIN = ('B', 'S')
+
+
+def fix_tag(tag, last):
+    """Ensure a valid IOB sequence."""
+    if tag in OUTSIDE:
+        tag = OUTSIDE[0]
+    else:
+        # Definitely use B or I, including something like "O-chemical".
+        tag, label = tag[0], tag[1:]
+        if tag in BEGIN or last in OUTSIDE or last[1:] != label:
+            tag = BEGIN[0]
+        else:
+            tag = INSIDE[0]
+        tag += label  # rejoin
+    return tag
 
 
 if __name__ == '__main__':
